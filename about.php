aea3abc26c46476c4366f944c7308d1b1fc4d975
vishwakalyanaraman <vishwa.kalyanaraman@gmail.com>
2018-04-05 01:17:37 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,77 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();} for php 5.4 and above
+
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About Us</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">Parv's Computer Store</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li class="active"><a href="about.php">About</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+    
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+
+
+
+    <div class="row" style="margin-top:30px;">
+      <div class="small-12">
+        <p>Parv's computer store is a project done for our DBMS lab evaluation. On this website, you can place orders for various computers and purchase them from your shopping cart.</p>
+
+        <footer>
+           <p style="text-align:center; font-size:0.8em;">&copy; Parv's Computer Store. All Rights Reserved.</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
