6321289bd6b6fb038a2a3193d71810c3ee354b54
vishwakalyanaraman <vishwa.kalyanaraman@gmail.com>
2018-04-05 13:28:43 +0530

    Add files via upload

--- cart.php
+++ cart.php
@@ -13,7 +13,7 @@ include 'config.php';
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Shopping Cart</title>
+    <title>Your Cart</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
